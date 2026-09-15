ffa2e4fd1b513b74748bf24021a8e0561160b0ba
INJOO <injooinjoo@gmail.com>
2026-09-15 21:01:55 +0900

    Publish apps/site from injooinjoo/sidekick@94ff3d143395: add privacy-safe GA4 traffic measurement

---
+++ analytics.js
@@ -0,0 +1,16 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  window.dataLayer.push(arguments);
+}
+
+gtag('consent', 'default', {
+  analytics_storage: 'denied',
+  ad_storage: 'denied',
+  ad_user_data: 'denied',
+  ad_personalization: 'denied',
+});
+gtag('js', new Date());
+gtag('config', 'G-GMGMBQR225', {
+  allow_google_signals: false,
+  allow_ad_personalization_signals: false,
+});
